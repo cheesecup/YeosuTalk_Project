d44a7b732936721671983ff5e5fa1559e2499cd1
cheesecup <kdy8252@naver.com>
2022-12-06 01:40:02 +0900

    data.sql 사용을 위한 설정 추가 및 sql문 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+# member dummy data by http://www.mockaroo.com
+insert into member (id, account_id, pwd, name, citizen) values (null, 'kiddons0@google.co.uk', 'Edgepulse', 'lmarciek0', 'citizen');
+insert into member (id, account_id, pwd, name, citizen) values (null, 'ameffin1@vkontakte.ru', 'Devbug', 'mcamoletto1', 'citizen');
+insert into member (id, account_id, pwd, name, citizen) values (null, 'gmeekins2@godaddy.com', 'Flashset', 'acasone2', 'citizen');
+
+# tour_board dummy data by http://www.mockaroo.com
+insert into tour_board (id, content, title, writer) values (null, 'Revision of Nonautologous Tissue Substitute in Abdominal Wall, Percutaneous Approach', 'Carts of Darkness', 'mlerhinan0');
+insert into tour_board (id, content, title, writer) values (null, 'Bypass Left Greater Saphenous Vein to Lower Vein with Synthetic Substitute, Open Approach', 'A Pistol For Ringo', 'pdoberer1');
+insert into tour_board (id, content, title, writer) values (null, 'Release Right Renal Artery, Percutaneous Endoscopic Approach', 'Last Exit to Brooklyn', 'mminchi2');
+insert into tour_board (id, content, title, writer) values (null, 'Drainage of Lumbar Sympathetic Nerve with Drainage Device, Percutaneous Endoscopic Approach', 'Picnic at Hanging Rock', 'pfaulconer3');
+insert into tour_board (id, content, title, writer) values (null, 'Fragmentation in Lingula Bronchus, Percutaneous Approach', 'First Man Into Space', 'jpacheco4');
+insert into tour_board (id, content, title, writer) values (null, 'Replacement of Right Hand Tendon with Nonautologous Tissue Substitute, Percutaneous Endoscopic Approach', 'JFK: The Smoking Gun', 'agoodhall5');
+insert into tour_board (id, content, title, writer) values (null, 'Supplement Right Foot Tendon with Autologous Tissue Substitute, Open Approach', '2001: A Space Odyssey', 'gcansfield6');
+insert into tour_board (id, content, title, writer) values (null, 'Insertion of Monitoring Device into Right Lung, Via Natural or Artificial Opening Endoscopic', 'Paul Williams Still Alive', 'tdenniss7');
+insert into tour_board (id, content, title, writer) values (null, 'Removal of External Fixation Device from Right Lower Femur, Percutaneous Endoscopic Approach', 'William Shakespeare''s Romeo + Juliet', 'kbonallick8');
+insert into tour_board (id, content, title, writer) values (null, 'Bypass Upper Esophagus to Cutaneous with Nonautologous Tissue Substitute, Via Natural or Artificial Opening Endoscopic', 'Children, The', 'lmussetti9');
