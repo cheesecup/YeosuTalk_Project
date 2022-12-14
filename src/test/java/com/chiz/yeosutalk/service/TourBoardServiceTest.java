f45a734ffa076d981756f160c0928d40d5256bc9
cheesecup <kdy8252@naver.com>
2022-12-14 22:30:42 +0900

    관광 게시판 게시글 수정,조회 테스트 코드 추가

--- src/test/java/com/chiz/yeosutalk/service/TourBoardServiceTest.java
+++ src/test/java/com/chiz/yeosutalk/service/TourBoardServiceTest.java
@@ -1,13 +1,14 @@
 package com.chiz.yeosutalk.service;
 
 import com.chiz.yeosutalk.domain.TourBoard;
+import com.chiz.yeosutalk.dto.TourBoardDto;
 import com.chiz.yeosutalk.dto.TourBoardFormDto;
 import com.chiz.yeosutalk.repository.TourBoardRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
@@ -15,15 +16,16 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class TourBoardServiceTest {
 
     @Autowired TourBoardService tourBoardService;
     @Autowired TourBoardRepository tourBoardRepository;
 
-    @BeforeEach
-    void before() {
-        tourBoardRepository.deleteAll();
-    }
+//    @BeforeEach
+//    void before() {
+//        tourBoardRepository.deleteAll();
+//    }
 
     @Test
     @DisplayName("관광 게시판 게시글 저장 서비스 테스트")
@@ -43,16 +45,36 @@ class TourBoardServiceTest {
     @DisplayName("관광 게시판 게시글 전체 조회")
     void findAllTourBoardList() {
         //Given
-        for (int i=0; i< 5; i++) {
-            TourBoardFormDto tourBoardFormDto = new TourBoardFormDto("title"+i, "content"+i, "writer"+i);
-            TourBoard tourBoard = TourBoard.toEntity(tourBoardFormDto);
-            tourBoardRepository.save(tourBoard);
-        }
+        long count = tourBoardRepository.count();
 
         //When
         List<TourBoard> tourBoardList = tourBoardService.tourBoardList();
 
         //Given
-        assertThat(tourBoardList.size()).isEqualTo(5);
+        assertThat(tourBoardList.size()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("관광 게시판 게시글 상세 조회 서비스 테스트")
+    void findTourBoardPostByIdTest() {
+        //When
+        TourBoardDto tourBoardDto = tourBoardService.postInfo(1L);
+
+        //Then
+        assertThat(tourBoardDto.getWriter()).isEqualTo("mlerhinan0");
+        System.out.println(tourBoardDto.getContent());
+    }
+
+    @Test
+    @DisplayName("관광 게시판 게시글 수정 서버스 테스트")
+    void updateTourPostTest() {
+        TourBoardFormDto tourBoardFormDto = new TourBoardFormDto("updateTitle", "updateContent", "mlerhinan0");
+        TourBoard tourBoard = tourBoardRepository.findById(1L).orElseThrow(() -> new EntityNotFoundException("해당 게시글을 찾을 수 없습니다."));
+
+        //When
+        Long updateId = tourBoardService.updateTourPost(1L, tourBoardFormDto);
+
+        //Then
+        assertThat(updateId).isEqualTo(1L);
     }
 }
