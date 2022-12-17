ec736d0059d66af47c7122bd26f36fe7fea06074
cheesecup <kdy8252@naver.com>
2022-12-17 21:28:14 +0900

    댓글 생성 데이터 SQL 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,20 @@
-insert into member (id, account_id, pwd, name, citizen) values (null, 'kiddons0@google.co.uk', 'Edgepulse', 'lmarciek0', 'citizen');
-insert into member (id, account_id, pwd, name, citizen) values (null, 'ameffin1@vkontakte.ru', 'Devbug', 'mcamoletto1', 'citizen');
-insert into member (id, account_id, pwd, name, citizen) values (null, 'gmeekins2@godaddy.com', 'Flashset', 'acasone2', 'citizen');
+insert into member (member_id, account_id, pwd, name, nickname, citizen) values (null, 'kiddons0@google.co.uk', 'Edgepulse', 'lmarciek0', 'kiddons' , 'yes');
+insert into member (member_id, account_id, pwd, name, nickname, citizen) values (null, 'ameffin1@vkontakte.ru', 'Devbug', 'mcamoletto1', 'ameffin1', 'non');
+insert into member (member_id, account_id, pwd, name, nickname, citizen) values (null, 'gmeekins2@godaddy.com', 'Flashset', 'acasone2', 'gmeekins2' , 'yes');
 
-insert into tour_board (id, content, title, writer) values (null, 'Revision of Nonautologous Tissue Substitute in Abdominal Wall, Percutaneous Approach', 'Carts of Darkness', 'mlerhinan0');
-insert into tour_board (id, content, title, writer) values (null, 'Bypass Left Greater Saphenous Vein to Lower Vein with Synthetic Substitute, Open Approach', 'A Pistol For Ringo', 'pdoberer1');
-insert into tour_board (id, content, title, writer) values (null, 'Release Right Renal Artery, Percutaneous Endoscopic Approach', 'Last Exit to Brooklyn', 'mminchi2');
-insert into tour_board (id, content, title, writer) values (null, 'Drainage of Lumbar Sympathetic Nerve with Drainage Device, Percutaneous Endoscopic Approach', 'Picnic at Hanging Rock', 'pfaulconer3');
-insert into tour_board (id, content, title, writer) values (null, 'Fragmentation in Lingula Bronchus, Percutaneous Approach', 'First Man Into Space', 'jpacheco4');
-insert into tour_board (id, content, title, writer) values (null, 'Replacement of Right Hand Tendon with Nonautologous Tissue Substitute, Percutaneous Endoscopic Approach', 'JFK: The Smoking Gun', 'agoodhall5');
-insert into tour_board (id, content, title, writer) values (null, 'Supplement Right Foot Tendon with Autologous Tissue Substitute, Open Approach', '2001: A Space Odyssey', 'gcansfield6');
-insert into tour_board (id, content, title, writer) values (null, 'Insertion of Monitoring Device into Right Lung, Via Natural or Artificial Opening Endoscopic', 'Paul Williams Still Alive', 'tdenniss7');
-insert into tour_board (id, content, title, writer) values (null, 'Removal of External Fixation Device from Right Lower Femur, Percutaneous Endoscopic Approach', 'William Shakespeare''s Romeo + Juliet', 'kbonallick8');
-insert into tour_board (id, content, title, writer) values (null, 'Bypass Upper Esophagus to Cutaneous with Nonautologous Tissue Substitute, Via Natural or Artificial Opening Endoscopic', 'Children, The', 'lmussetti9');
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Wakunai Airport', 'eu massa donec dapibus duis at velit eu est congue elementum in hac', 'kiddons', 1);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Yasouj Airport', 'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet', 'ameffin1', 2);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Albacete-Los Llanos Airport', 'id ornare imperdiet sapien urna pretium nisl ut volutpat sapien', 'gmeekins2', 3);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Loani Airport', 'curae nulla dapibus dolor vel est donec odio justo sollicitudin ut suscipit a', 'kiddons', 1);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Nottingham Airport', 'vestibulum ante ipsum primis in faucibus orci luctus et ultrices', 'kiddons', 1);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Townsville Airport', 'tempus vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus', 'kiddons', 1);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'RNAS Yeovilton', 'in lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu', 'ameffin1', 2);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Pittsburgh International Airport', 'sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris', 'ameffin1', 2);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Eniwetok Airport', 'ultrices posuere cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui nec nisi', 'kiddons', 1);
+insert into tour_board (tour_board_id, title, content, writer, member_id) values (null, 'Peterborough Airport', 'magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt', 'kiddons', 1);
+
+insert into comments (tour_board_comments_id, comment, commenter, tour_board_id, created_at, modified_at) values (null, 'Notechis semmiannulatus', 'gmeekins', 2, '2022-12-17 19:16:08.882000', '2022-12-17 19:16:08.882305');
+insert into comments (tour_board_comments_id, comment, commenter, tour_board_id, created_at, modified_at) values (null, 'Nucifraga columbiana', 'gmeekins', 1, '2022-12-16 19:16:08.882000', '2022-12-16 19:16:08.882305');
+insert into comments (tour_board_comments_id, comment, commenter, tour_board_id, created_at, modified_at) values (null, 'Eudyptula minor', 'gmeekins', 2, '2022-12-13 19:16:08.882000', '2022-12-13 19:16:08.882305');
+insert into comments (tour_board_comments_id, comment, commenter, tour_board_id, created_at, modified_at) values (null, 'Graspus graspus', 'ameffin1', 3, '2022-12-11 19:16:08.882000', '2022-12-11 19:16:08.882305');
+insert into comments (tour_board_comments_id, comment, commenter, tour_board_id, created_at, modified_at) values (null, 'Felis serval', 'gmeekins', 1, '2022-12-12 19:16:08.882000', '2022-12-12 19:16:08.882305');
